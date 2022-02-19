21703d66197b480bb54ecb25fb7de5413b06f4dd
Steven Chen <junyich@uw.edu>
2022-02-18 22:38:37 -0800

    Modified summary.R and the summary paragraph in index.rmd

--- source/Summary.R
+++ source/Summary.R
@@ -1,12 +1,19 @@
-source("drug_data.R")
-source("CarModel_Accidents.R")
-source("fatality_relating_restraint.R")
-source("Route_classification.R")
+source("../source/drug_data.R")
+source("../source/CarModel_Accidents.R")
+source("../source/fatality_relating_restraint.R")
+source("../source/Route_classification.R")
 
+fat_2017 <- fatality$Rate2017
+fat_2018 <- fatality$Rate2018
+fat_2019 <- fatality$Rate2019
 summary <- list(
   propotion,
   highest_death_place, 
   top_five_df$MAKENAME,
   total_incident,
-  (fatality$Rate2017/fatality$Rate2018/ fatality$Rate2019)
-)
+  fatality$Type,
+  fat_2017,
+  fat_2018,
+  fat_2019
+  )
+
