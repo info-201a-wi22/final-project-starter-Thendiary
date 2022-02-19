73e88878ba15c7054f388e8d42b1de1aff1a9073
thendiary <yuhanz15@uw.edu>
2022-02-18 21:24:57 -0800

    edit

---
+++ source/Summary.R
@@ -0,0 +1,9 @@
+source("drug_data.R")
+source("CarModel_Accidents.R")
+source("fatality_relating_restraint.R")
+source("Route_classification.R")
+
+summary <- list(
+  propotion,
+  highest_death_place
+)
