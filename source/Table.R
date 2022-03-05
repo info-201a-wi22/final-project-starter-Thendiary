cf19a0f69501281daad8c30cad00067131dd1aef
Junyi Chen <junyich@uw.edu>
2022-03-05 13:02:36 -0800

    Changed the structure of Rmd; Revised Table.R

--- source/Table.R
+++ source/Table.R
@@ -2,32 +2,12 @@ library("dplyr")
 library("tidyverse")
 
 accident_2019 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/accident.CSV")
-accident_2018 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/accident.CSV")
-accident_2017 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2017NationalCSV/accident.CSV")
 
-Person2017<- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2017NationalCSV/Person.CSV")
-Person2018 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Person.CSV")
 Person2019 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Person.CSV")
 
-vehicle_2017 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2017NationalCSV/Vehicle.csv")
-vehicle_2018 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Vehicle.csv")
 vehicle_2019 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Vehicle.csv")
 
-Drugs_2018 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Drugs.CSV")
 Drugs_2019 <- read.csv("../data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Drugs.CSV")
-
-table_2017 <- left_join(accident_2017, Person2017, by = "ST_CASE") %>%
-  left_join(vehicle_2017, by = "ST_CASE") %>%
-  select(REST_USENAME, DOANAME, ROUTENAME, FATALS, ST_CASE, MAKENAME.x) %>%
-  mutate(Year = 2017, DRUGRESNAME = NA)
-table_2017 <- table_2017[1:10000,]
-
-table_2018 <- left_join(accident_2018, Person2018, by = "ST_CASE") %>%
-  left_join(vehicle_2018, by = "ST_CASE") %>%
-  left_join(Drugs_2018, by = "ST_CASE") %>%
-  select(REST_USENAME, DOANAME, ROUTENAME, FATALS, ST_CASE, DRUGRESNAME, MAKENAME.x) %>%
-  mutate(Year = 2018)
-table_2018 <- table_2018[1:10000,]
 
 table_2019 <- left_join(accident_2019, Person2019, by = "ST_CASE") %>%
   left_join(vehicle_2019, by = "ST_CASE") %>%
@@ -36,7 +16,10 @@ table_2019 <- left_join(accident_2019, Person2019, by = "ST_CASE") %>%
   mutate(Year = 2019)
 table_2019 <- table_2019[1:10000,]
 
-table_total <- rbind(table_2017, table_2018, table_2019) %>%
-  group_by(Year)
+table_2019 <- table_2019 %>% 
+  select(Year, ST_CASE, everything()) %>% 
+  group_by(ST_CASE) %>% 
+  filter(MAKENAME.x == unique(MAKENAME.x)) %>% 
+  filter(ST_CASE == 10002)
 
-colnames(table_total) <- c('Use of Restraint', 'Death Type', 'Route Type', 'Num of Death', 'Case Number', 'Auto Maker', 'Year', 'Use of Drug')
+colnames(table_2019) <- c('Year', 'Case_ID', 'Use_of_Restraint', 'Death_Type', 'Route_Type', 'Num_Death', 'Drug_Use', 'Auto_Maker')
