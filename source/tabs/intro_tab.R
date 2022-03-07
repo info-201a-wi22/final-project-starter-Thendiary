d58a42a1d8ee2761592fc8542ba20f9dbd07a681
Junyi Chen <junyich@uw.edu>
2022-03-07 14:53:06 -0800

    Finsih intro

--- source/tabs/intro_tab.R
+++ source/tabs/intro_tab.R
@@ -6,19 +6,19 @@ intro <- tabPanel(
   fluidRow(
     column(2),
     column(8,
-           HTML("<p>&nbsp;&nbsp;&nbsp;&nbsp;
+           HTML("<br>
+           <p>&nbsp;&nbsp;&nbsp;&nbsp;
            We will analyze fatal car accidents from <strong>four main aspects</strong>: <i>Restraint, Drugs, Road type, and automaker</i> from 2017 to 2019 in the US. 
            <strong>Using Restraint</strong> is especially important on the car which is calculated by Spot Death Rate elating using retraint from 2017 to 2019. 
            We are also concerned about the <strong>type of road</strong> where rashes occured, because the roughness of the road also has some impact on the cause of car accidents. 
            <strong>Car models produced by different makers</strong> usually yield different results when encountring car crashes. 
            By evaluating how each automaker performs in fatal crashes, we will be able to model the typical characteristics of crashes and how their products protect the driver and passengers when accident takes place. 
-           For addictive driving, we analysis the data of drug use in severe car accidents, which turns out that nearly half of people, who was tested for drugs, is under the effect of addictive drugs while they encountered car crashes. 
-           People are using them without right recognition about drugs, heading to what them think is \"happiness\" and \"freedom\". 
-           We advise all drivers to make sure you are in clear consciousness while you are driving, or it is easy to get involved in car crashes with serious consequence.</p>"),
+           For these years, the most shocking news in U.S. about drug is the set up of pilot safe injection sites in Rhode Island, which makes this state become the first state in U.S. with legal injecion sites of drugs. 
+           For this reason, combined with the data sets available, we choose addictive driving as one of our topics in this project. 
+           When we first thought about this question, we just focused on the proportion of people whose test result is positive or negative, and tried to find if there is a clear result from this analysis. 
+           With our analysis went deeper, we found that sometimes drugs themself are not the reason of the growing population addicted in drugs, but the opinion is.</p>"),
            
-           HTML("<p>&nbsp;&nbsp;&nbsp;&nbsp;
-           ")),
-    column(2,
-           HTML("<p>&nbsp;&nbsp;&nbsp;&nbsp;"))
+           ),
+    column(2)
   )
 )
