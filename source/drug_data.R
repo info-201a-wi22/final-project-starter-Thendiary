c531c2aa8b95ac8453d91b488d6bee4e374ea024
echo ge <echoge@echodeMacBook-Pro.local>
2022-03-08 18:30:53 -0800

    Change the style

--- source/drug_data.R
+++ source/drug_data.R
@@ -5,68 +5,72 @@ library("stringr")
 library("treemapify")
 
 # Read csv files.
-Drugs_2018 <- read.csv(
+drugs_2018 <- read.csv(
   "data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Drugs.CSV")
-Drugs_2019 <- read.csv(
+drugs_2019 <- read.csv(
   "data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Drugs.CSV")
 
 # Filter out important information.
-Drugs_tested_2018 <- Drugs_2018 %>%
+drugs_tested_2018 <- drugs_2018 %>%
   select(STATENAME, DRUGSPECNAME, DRUGRESNAME) %>%
   filter(DRUGRESNAME != "Test Not Given")
-Drugs_tested_2019 <- Drugs_2019 %>%
+drugs_tested_2019 <- drugs_2019 %>%
   select(STATENAME, DRUGSPECNAME, DRUGRESNAME) %>%
   filter(DRUGRESNAME != "Test Not Given")
 
 tested_2018 <- paste(
-  round(nrow(Drugs_tested_2018) / nrow(Drugs_2018) * 100, 2),"%", sep = ""
+  round(nrow(Drugs_tested_2018) / nrow(Drugs_2018) * 100, 2), "%", sep = ""
   )
 tested_2019 <- paste(
-  round(nrow(Drugs_tested_2019) / nrow(Drugs_2019) * 100, 2),"%", sep = ""
+  round(nrow(Drugs_tested_2019) / nrow(Drugs_2019) * 100, 2), "%", sep = ""
 )
 
 # Found how many positive cases.
-positive_test_2018 <- Drugs_tested_2018 %>%
+positive_test_2018 <- drugs_tested_2018 %>%
   filter(DRUGRESNAME != "Tested, No Drugs Found/Negative")
-positive_test_2019 <- Drugs_tested_2019 %>%
+positive_test_2019 <- drugs_tested_2019 %>%
   filter(DRUGRESNAME != "Tested, No Drugs Found/Negative")
 
 # Percentage that with negative drug test result.
-drug_found_negative_2018 <- 100 - nrow(positive_test_2018) / 
-  nrow(Drugs_tested_2018) * 100
-drug_found_negative_2019 <- 100 - nrow(positive_test_2019) / 
-  nrow(Drugs_tested_2019) * 100
+drug_found_negative_2018 <- 100 - nrow(positive_test_2018) /
+  nrow(drugs_tested_2018) * 100
+drug_found_negative_2019 <- 100 - nrow(positive_test_2019) /
+  nrow(drugs_tested_2019) * 100
 
 # Get all percentage of different kind of drugs.
-positive_test_2018$DRUGRESNAME <- tolower(positive_test_2018$DRUGRESNAME) 
+positive_test_2018$DRUGRESNAME <- tolower(positive_test_2018$DRUGRESNAME)
 positive_test_2018$DRUGRESNAME <-
-  if_else(str_detect(positive_test_2018$DRUGRESNAME,"unknown"), "other drug", positive_test_2018$DRUGRESNAME)
+  if_else(str_detect(positive_test_2018$DRUGRESNAME, "unknown"), "other drug",
+          positive_test_2018$DRUGRESNAME)
 drug_percentage_2018 <- positive_test_2018 %>%
   mutate(count = 1) %>%
   group_by(DRUGRESNAME) %>%
   summarise(num = sum(count)) %>%
   filter(DRUGRESNAME != "not reported")
 
-positive_test_2019$DRUGRESNAME <- tolower(positive_test_2019$DRUGRESNAME) 
+positive_test_2019$DRUGRESNAME <- tolower(positive_test_2019$DRUGRESNAME)
 positive_test_2019$DRUGRESNAME <-
-  if_else(str_detect(positive_test_2019$DRUGRESNAME,"unknown"), "other drug", positive_test_2019$DRUGRESNAME)
+  if_else(str_detect(positive_test_2019$DRUGRESNAME, "unknown"), "other drug",
+          positive_test_2019$DRUGRESNAME)
 drug_percentage_2019 <- positive_test_2019 %>%
   mutate(count = 1) %>%
   group_by(DRUGRESNAME) %>%
   summarise(num = sum(count)) %>%
-  filter(DRUGRESNAME != "not reported") 
+  filter(DRUGRESNAME != "not reported")
 
 # Summary of dataframe above.
-drug_test <- left_join(drug_percentage_2018, drug_percentage_2019, by = "DRUGRESNAME") %>%
+drug_test <- left_join(drug_percentage_2018, drug_percentage_2019,
+                       by = "DRUGRESNAME") %>%
   mutate(num = num.x + num.y) %>%
   select(DRUGRESNAME, num) %>%
-  mutate(percentage = paste(round(num / sum(num, na.rm = T) * 100, 2), "%", sep = "")) %>%
+  mutate(percentage = paste(round(num / sum(num, na.rm = T) * 100, 2), "%",
+                            sep = "")) %>%
   arrange(-num)
 drug_test <- drug_test[1:10, ]
 
 # Change drug name since some of them is too long.
-drug_name <- c("Other drug", "Marijuana", "Desoxyn", "Amphetamine", 
-               "Delta 9", "Benzoylecgonine", "Cocaine", "Fentanyl", 
+drug_name <- c("Other drug", "Marijuana", "Desoxyn", "Amphetamine",
+               "Delta 9", "Benzoylecgonine", "Cocaine", "Fentanyl",
                "Alprazolam", "Morphine")
 drug_test$DRUGRESNAME <- drug_name
 
@@ -75,12 +79,11 @@ propotion <- drug_test %>%
   pull(percentage)
 
 # Plot
-plot_drug <- ggplot(drug_test, aes(area = num, fill = DRUGRESNAME, label = percentage)) +
+plot_drug <- ggplot(drug_test, aes(area = num, fill = DRUGRESNAME,
+                                   label = percentage)) +
   geom_treemap() +
-  geom_treemap_text(size = 15, 
-                    color = "white", 
+  geom_treemap_text(size = 15,
+                    color = "white",
                     place = "centre") +
   ggtitle("Drug specification for 2018-2019")
 plot_drug
-
-
--- source/fatality_relating_restraint.R
+++ source/fatality_relating_restraint.R
@@ -2,19 +2,25 @@ library("dplyr")
 library("tidyverse")
 library("ggplot2")
 
-Person2017<- read.csv("data/Carccident/Fatal_vehicle_crashes/FARS2017NationalCSV/Person.CSV")
-Person2018 <- read.csv("data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Person.CSV")
-Person2019 <- read.csv("data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Person.CSV")
+person2017 <- read.csv(
+  "data/Carccident/Fatal_vehicle_crashes/FARS2017NationalCSV/Person.CSV")
+person2018 <- read.csv(
+  "data/Carccident/Fatal_vehicle_crashes/FARS2018NationalCSV/Person.CSV")
+person2019 <- read.csv(
+  "data/Carccident/Fatal_vehicle_crashes/FARS2019NationalCSV/Person.CSV")
 
+library("lintr")
+lint("fatality_relating_restraint.R")
 
 # the died at scene rate for not using restraint system in 2017
-num_total_norest2017 <- Person2017 %>%
+num_total_norest2017 <- person2017 %>%
   select("REST_USENAME", "DOANAME") %>%
-  filter(REST_USENAME == "None Used / Not Applicable")%>%
+  filter(REST_USENAME == "None Used / Not Applicable") %>%
   nrow()
 
-num_notuse_died_at_scene2017 <- Person2017 %>%
-  filter(REST_USENAME == "None Used / Not Applicable", DOANAME == "Died at Scene")%>%
+num_notuse_died_at_scene2017 <- person2017 %>%
+  filter(REST_USENAME == "None Used / Not Applicable",
+         DOANAME == "Died at Scene") %>%
   nrow()
 num_notuse_died_at_scene2017
 
@@ -24,41 +30,44 @@ norest_deathrate2017
 
 # the died at scene rate for using Shoulder and Lap Belt in 2017 .
 
-num_total_use_belt2017 <- Person2017 %>%
-  filter(REST_USENAME == "Shoulder and Lap Belt Used")%>%
+num_total_use_belt2017 <- person2017 %>%
+  filter(REST_USENAME == "Shoulder and Lap Belt Used") %>%
   nrow()
 num_total_use_belt2017
 
-num_use_belt_died_at_scene2017 <- Person2017 %>%
-  filter(REST_USENAME == "Shoulder and Lap Belt Used", DOANAME == "Died at Scene")%>%
+num_use_belt_died_at_scene2017 <- person2017 %>%
+  filter(REST_USENAME == "Shoulder and Lap Belt Used",
+         DOANAME == "Died at Scene") %>%
   nrow()
 num_use_belt_died_at_scene2017
 
-use_belt_deathrate2017 <- num_use_belt_died_at_scene2017 / num_total_use_belt2017
+use_belt_deathrate2017 <- num_use_belt_died_at_scene2017 /
+  num_total_use_belt2017
 use_belt_deathrate2017
 
 # the died at scene rate for using Booster Seat in 2017
 
-num_total_use_booster_seat2017 <- Person2017 %>%
-  filter(REST_USENAME == "Booster Seat")%>%
+num_total_use_booster_seat2017 <- person2017 %>%
+  filter(REST_USENAME == "Booster Seat") %>%
   nrow()
 num_total_use_booster_seat2017
 
-num_use_booster_seat_died_at_scene2017 <- Person2017 %>%
+num_use_booster_seat_died_at_scene2017 <- person2017 %>%
   filter(REST_USENAME == "Booster Seat", DOANAME == "Died at Scene")%>%
   nrow()
 num_use_booster_seat_died_at_scene2017
 
-use_booster_seat_deathrate2017 <- num_use_booster_seat_died_at_scene2017 / num_total_use_booster_seat2017
+use_booster_seat_deathrate2017 <- num_use_booster_seat_died_at_scene2017 /
+  num_total_use_booster_seat2017
 use_booster_seat_deathrate2017
 
 # the died at scene rate for not using restraint system in 2018
-num_total_norest2018 <- Person2018 %>%
+num_total_norest2018 <- person2018 %>%
   filter(REST_USENAME == "None Used / Not Applicable")%>%
   nrow()
 num_total_norest2018
 
-num_notuse_died_at_scene2018 <- Person2018 %>%
+num_notuse_died_at_scene2018 <- person2018 %>%
   select("REST_USENAME", "DOANAME") %>%
   filter(REST_USENAME == "None Used / Not Applicable", DOANAME == "Died at Scene")%>%
   nrow()
@@ -69,42 +78,44 @@ norest_deathrate2018
 
 # the died at scene rate for using Shoulder and Lap Belt in 2018
 
-num_total_use_belt2018 <- Person2018 %>%
+num_total_use_belt2018 <- person2018 %>%
   filter(REST_USENAME == "Shoulder and Lap Belt Used")%>%
   nrow()
 num_total_use_belt2018
 
-num_use_belt_died_at_scene2018 <- Person2018 %>%
+num_use_belt_died_at_scene2018 <- person2018 %>%
   filter(REST_USENAME == "Shoulder and Lap Belt Used", DOANAME == "Died at Scene")%>%
   nrow()
 num_use_belt_died_at_scene2018
 
-use_belt_deathrate2018 <- num_use_belt_died_at_scene2018 / num_total_use_belt2018
+use_belt_deathrate2018 <- num_use_belt_died_at_scene2018 /
+  num_total_use_belt2018
 use_belt_deathrate2018
 
 # the died at scene rate for using Booster Seat in 2018
 
-num_total_use_booster_seat2018 <- Person2018 %>%
+num_total_use_booster_seat2018 <- person2018 %>%
   filter(REST_USENAME == "Booster Seat")%>%
   nrow()
 num_total_use_booster_seat2018
 
-num_use_booster_seat_died_at_scene2018 <- Person2018 %>%
+num_use_booster_seat_died_at_scene2018 <- person2018 %>%
   filter(REST_USENAME == "Booster Seat", DOANAME == "Died at Scene")%>%
   nrow()
 num_use_booster_seat_died_at_scene2018
 
-use_booster_seat_deathrate2018 <- num_use_booster_seat_died_at_scene2018 / num_total_use_booster_seat2018
+use_booster_seat_deathrate2018 <- num_use_booster_seat_died_at_scene2018 /
+  num_total_use_booster_seat2018
 use_booster_seat_deathrate2018
 
 # the died at scene rate for not using restraint system in 2019
-num_total_norest2019 <- Person2019 %>%
+num_total_norest2019 <- person2019 %>%
   select("REST_USENAME", "DOANAME") %>%
   filter(REST_USENAME == "None Used/Not Applicable")%>%
   nrow()
 num_total_norest2019
 
-num_notuse_died_at_scene2019 <- Person2019 %>%
+num_notuse_died_at_scene2019 <- person2019 %>%
   filter(REST_USENAME == "None Used/Not Applicable", DOANAME == "Died at Scene")%>%
   nrow()
 num_notuse_died_at_scene2019
@@ -114,12 +125,12 @@ norest_deathrate2019
 
 # the died at scene rate for using Shoulder and Lap Belt in 2019
 
-num_total_use_belt2019 <- Person2019 %>%
+num_total_use_belt2019 <- person2019 %>%
   filter(REST_USENAME == "Shoulder and Lap Belt Used")%>%
   nrow()
 num_total_use_belt2019
 
-num_use_belt_died_at_scene2019 <- Person2019 %>%
+num_use_belt_died_at_scene2019 <- person2019 %>%
   filter(REST_USENAME == "Shoulder and Lap Belt Used", DOANAME == "Died at Scene")%>%
   nrow()
 num_use_belt_died_at_scene2019
@@ -129,44 +140,52 @@ use_belt_deathrate2019
 
 # the died at scene rate for using Booster Seat in 2019
 
-num_total_use_booster_seat2019 <- Person2019 %>%
+num_total_use_booster_seat2019 <- person2019 %>%
   filter(REST_USENAME == "Booster Seat")%>%
   nrow()
 num_total_use_booster_seat2019
 
-num_use_booster_seat_died_at_scene2019 <- Person2019 %>%
+num_use_booster_seat_died_at_scene2019 <- person2019 %>%
   filter(REST_USENAME == "Booster Seat", DOANAME == "Died at Scene")%>%
   nrow()
 num_use_booster_seat_died_at_scene2019
 
-use_booster_seat_deathrate2019 <- num_use_booster_seat_died_at_scene2019 / num_total_use_booster_seat2019
+use_booster_seat_deathrate2019 <- num_use_booster_seat_died_at_scene2019 /
+  num_total_use_booster_seat2019
 use_booster_seat_deathrate2019
 
 # create data frame
 Type <- c("No Restraint", "Using Shoulder and Lap Belt", "Using Booster Seat")
-Rate2017 <- c(round(norest_deathrate2017, digits = 4), round(use_belt_deathrate2017, digits = 4),round(use_booster_seat_deathrate2017, digits = 4))
-Rate2018 <- c(round(norest_deathrate2018, digits = 4), round(use_belt_deathrate2018, digits = 4),round(use_booster_seat_deathrate2018, digits = 4))
-Rate2019 <- c(round(norest_deathrate2019, digits = 4), round(use_belt_deathrate2019, digits = 4),round(use_booster_seat_deathrate2019, digits = 4))
-fatality <- data.frame(Type, Rate2017,Rate2018, Rate2019)
+Rate2017 <- c(round(norest_deathrate2017, digits = 4),
+              round(use_belt_deathrate2017, digits = 4),
+              round(use_booster_seat_deathrate2017, digits = 4))
+Rate2018 <- c(round(norest_deathrate2018, digits = 4),
+              round(use_belt_deathrate2018, digits = 4),
+              round(use_booster_seat_deathrate2018, digits = 4))
+Rate2019 <- c(round(norest_deathrate2019, digits = 4),
+              round(use_belt_deathrate2019, digits = 4),
+              round(use_booster_seat_deathrate2019, digits = 4))
+fatality <- data.frame(Type, Rate2017, Rate2018, Rate2019)
 
 # draw the plot
-ggplot(fatality, aes(x=Type, y=Rate2017))+
+ggplot(fatality, aes(x = Type, y = Rate2017))+
   geom_bar(stat='identity')+
   ylab("2017 - Spot Death Rate")+
   xlab("Using Restraint")
 
 
-ggplot(fatality, aes(x=Type, y=Rate2018))+
+ggplot(fatality, aes(x = Type, y = Rate2018))+
   geom_bar(stat='identity')+
   ylab("2018 - Spot Death Rate")+
   xlab("Using Restraint")
 
-ggplot(fatality, aes(x=Type, y=Rate2019))+
+ggplot(fatality, aes(x = Type, y = Rate2019))+
   geom_bar(stat='identity')+
   ylab("2019 - Spot Death Rate")+
   xlab("Using Restraint")
 
-fatalitylong <- gather(fatality, key="measure", value="value", c("Rate2017", "Rate2018", "Rate2019"))
+fatalitylong <- gather(fatality, key = "measure", value = "value",
+                       c("Rate2017", "Rate2018", "Rate2019"))
 
 plot_mortality <- ggplot(fatalitylong, aes(x = Type , y=value, fill = Type))+
   geom_bar(stat='identity')+
@@ -177,7 +196,7 @@ plot_mortality <- ggplot(fatalitylong, aes(x = Type , y=value, fill = Type))+
     x = "Restraints",
     y = "Spot Death Rate",
   )+
-  geom_text(aes(label=value), vjust=1.6, color="white", size=3.5)+
+  geom_text(aes(label = value), vjust = 1.6, color = "white", size = 3.5)+
   theme(axis.text.x = element_text(angle = -45, hjust = 0)) 
   
 plot_mortality
